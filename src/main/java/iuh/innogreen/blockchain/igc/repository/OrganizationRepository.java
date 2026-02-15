18eaa21b2b895324aa115bcc30dca073b87fd7ec
HuynhDucPhu2502 <huynhducphu2502@gmail.com>
2026-02-15 14:49:59 +0700

    feat(organization): Add organization management base
    feat(organization): Add organization management base
    
    Adds initial DTO, repository, and service components for organization management.
    Updates CertificateRepository to use JSpeicify @NonNull annotations for better nullability guarantees.

---
+++ src/main/java/iuh/innogreen/blockchain/igc/repository/OrganizationRepository.java
@@ -0,0 +1,14 @@
+package iuh.innogreen.blockchain.igc.repository;
+
+import iuh.innogreen.blockchain.igc.entity.Organization;
+import org.jspecify.annotations.NonNull;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+/**
+ * Admin 2/15/2026
+ *
+ **/
+@Repository
+public interface OrganizationRepository extends JpaRepository<@NonNull Organization, @NonNull Long> {
+}
---
+++ src/main/java/iuh/innogreen/blockchain/igc/service/organization/impl/OrganizationServiceImpl.java
@@ -0,0 +1,16 @@
+package iuh.innogreen.blockchain.igc.service.organization.impl;
+
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import org.springframework.stereotype.Service;
+
+/**
+ * Admin 2/15/2026
+ *
+ **/
+@Service
+@RequiredArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
+public class OrganizationServiceImpl {
+}
